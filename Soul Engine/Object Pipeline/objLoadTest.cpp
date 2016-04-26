6e6cae97a3b1dfa21b030bee7459d8fba2771f54
Louis Silvestro <silvel2@rpi.edu>
2016-04-26 11:45:06 -0400

    Add basis for tesselate function

--- Soul Engine/Object Pipeline/objLoadTest.cpp
+++ Soul Engine/Object Pipeline/objLoadTest.cpp
@@ -1,16 +1,13 @@
-#include "../Source Files/Engine Core/Object/ObjLoader.h"
-#include <iostream>
+#include"objLoadTest.h"
 
 int main() {
-	std::string inputFile = "../../object_pipeline/box.obj";
+	char* inputFile = "../../Box.obj";
 	std::string err;
 	std::vector<tinyobj::shape_t> shapes;
 	std::vector<tinyobj::material_t> materials;
-	tinyobj::LoadObj(shapes,materials,inputFile.c_str(),NULL);
-	if(!err.empty()) {
-		std::cout << err << std::endl;
-	}
-	else {
-		std::cout << ".obj file loaded successfully" << std::endl;
-	}
+	processFile(shapes, materials, inputFile);
+	tesselate(shapes, CENTIMETER*5);
+	getchar();
+	
+	return 0;
 }
--- Soul Engine/Object Pipeline/objLoadTest.h
+++ Soul Engine/Object Pipeline/objLoadTest.h
@@ -2,7 +2,9 @@
 #include <iostream>
 #include<vector>
 #include<string>
+#include "../Include Files/Metrics.h"
 
+//Load .obj file and populate shape and material vectors
 void processFile(std::vector<tinyobj::shape_t>& shapes, std::vector<tinyobj::material_t>& mat, char* file) {
 	std::string err;
 	err = tinyobj::LoadObj(shapes, mat, file, NULL);
@@ -11,5 +13,12 @@ void processFile(std::vector<tinyobj::shape_t>& shapes, std::vector<tinyobj::mat
 	}
 	else {
 		std::cout << "File loaded successfully" << std::endl;
+	}
+}
+
+//Tesselate the shapes into triangles whose size are given by argument "metric"
+void tesselate(const std::vector<tinyobj::shape_t>& shapes, float metric) {
+	for (size_t i = 0; i < shapes.size(); i++) {
+		
 	}
 }
