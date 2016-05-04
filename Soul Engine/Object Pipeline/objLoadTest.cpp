efefe88186279d4d0c5a3c5ad269629ce2dd17b3
Louis Silvestro <silvel2@rpi.edu>
2016-05-04 16:07:43 -0400

    Refactor some code in objLoadtest.h/cpp

--- Soul Engine/Object Pipeline/objLoadTest.cpp
+++ Soul Engine/Object Pipeline/objLoadTest.cpp
@@ -1,5 +1,24 @@
 #include"objLoadTest.h"
 
+//Load .obj file and populate shape and material vectors
+void processFile(std::vector<tinyobj::shape_t>& shapes, std::vector<tinyobj::material_t>& mat, char* file) {
+	std::string err;
+	err = tinyobj::LoadObj(shapes, mat, file, NULL);
+	if (!err.empty()) {
+		std::cout << err << std::endl;
+	}
+	else {
+		std::cout << "File loaded successfully" << std::endl;
+	}
+}
+
+//Tesselate the shapes into triangles whose size are given by argument "metric"
+void tesselate(const std::vector<tinyobj::shape_t>& shapes, float metric) {
+	for (size_t i = 0; i < shapes.size(); i++) {
+
+	}
+}
+
 int main() {
 	char* inputFile = "../../Box.obj";
 	std::string err;
--- Soul Engine/Object Pipeline/objLoadTest.h
+++ Soul Engine/Object Pipeline/objLoadTest.h
@@ -4,21 +4,9 @@
 #include<string>
 #include "../Include Files/Metrics.h"
 
-//Load .obj file and populate shape and material vectors
-void processFile(std::vector<tinyobj::shape_t>& shapes, std::vector<tinyobj::material_t>& mat, char* file) {
-	std::string err;
-	err = tinyobj::LoadObj(shapes, mat, file, NULL);
-	if (!err.empty()) {
-		std::cout << err << std::endl;
-	}
-	else {
-		std::cout << "File loaded successfully" << std::endl;
-	}
-}
+//Function for loading an .obj file. Output is stored in vars shapes and mat
+void processFile(std::vector<tinyobj::shape_t>& shapes, std::vector<tinyobj::material_t>& mat, char* file);
 
-//Tesselate the shapes into triangles whose size are given by argument "metric"
-void tesselate(const std::vector<tinyobj::shape_t>& shapes, float metric) {
-	for (size_t i = 0; i < shapes.size(); i++) {
-		
-	}
-}
+//Function for tesselating an object. Input is given by vector shapes and a metric to
+//Govern the size of the subtriangles
+void tesselate(const std::vector<tinyobj::shape_t>& shapes, float metric);
